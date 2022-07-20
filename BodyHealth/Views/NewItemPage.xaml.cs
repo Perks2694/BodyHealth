8b36c21e64d19bcdf7371e812e986f016421f5ec
Chris PerkinsYan <chris.a.perkins.yan@vanderbilt.edu>
2022-07-20 18:13:53 -0400

    Add project files.

---
+++ BodyHealth/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using BodyHealth.Models;
+using BodyHealth.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BodyHealth.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
